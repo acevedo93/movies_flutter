660a0e34c30d72186ec7c32318e935d36f66d10e
acevedo9303 <david.acevedo@vascarsolutions.com>
2022-09-22 15:15:17 -0500

    implementation models

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,31 @@ import 'package:provider/provider.dart';
 
 import 'screens/screens.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
 
 class AppState extends StatelessWidget {
   const AppState({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => MoviesProvider())
-    ])
+    return MultiProvider(
+        providers: [ChangeNotifierProvider(create: (_) => MoviesProvider(), lazy: false)],
+        child:const  MyApp());
   }
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      initialRoute:'home' ,
-      routes:   {
-        'home': ( _ ) =>  HomeScreen(),
-        'details': ( _ ) => DetailsScreen()
-      },
-      theme: ThemeData.dark().copyWith(
-        appBarTheme: const  AppBarTheme(
-          color:  Colors.blueGrey
-        )
-      ),
+      initialRoute: 'home',
+      routes: {'home': (_) => const  HomeScreen(), 'details': (_) => const DetailsScreen()},
+      theme: ThemeData.dark()
+          .copyWith(appBarTheme: const AppBarTheme(color: Colors.blueGrey)),
     );
   }
 }
--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,13 +1,25 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:movies/models/now_playing_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
+  final String _apiKey = '2eef05c086fcf6d9c34d52f54f47812d';
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _language = 'es-ES';
+
   MoviesProvider() {
+    
     print('Movies on display initialize');
     getOnDisplayMovies();
   }
 
-  getOnDisplayMovies() {
-    print('getOnDisplaymovies');
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movies/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+    final response = await http.get(url);
+    final nowplayingaaResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowplayingaaResponse.results[0]);
   }
 }
